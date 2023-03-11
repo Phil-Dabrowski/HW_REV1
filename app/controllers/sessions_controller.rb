b35f91e9f8e525441933aa93c54af8c55202f1d7
Phil-Dabrowski <phil.dabrowski@kellogg.northwestern.edu>
2023-03-11 14:07:49 +0000

    Changed the login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    @user = User.find_by({ "username" => params["username"] })
+    @user = User.find_by({ "email" => params["email"] })
     if @user
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
